e3f3dde9008860240ed88e432458eaf8176f154a
kozlov721isip <145563641+kozlov721isip@users.noreply.github.com>
2023-09-21 12:43:39 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,42 @@
+"use strict";
+let num1 = 1;
+let num2 = 2;
+console.log(num1 + num2);
+
+let a = 1;
+let b = 2;
+let c = 8;
+console.log (a + b + c);
+
+let num = '123';
+let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+console.log(sum);
+
+let num4 = 123;
+console.log(num4[1]);
+
+let a2 = 0;
+console.log(++a2);
+
+let num3 = 123;
+console.log(str.length);
+
+let str = '123';
+console.log(str.length);
+
+let a3 = 60 * 60 * 24;
+console.log(a3);
+
+let num5 = 123;
+let str1 = String(num5);
+console.log(str1[str1.length -1]);
+
+let a4 = '123';
+let b1 = '456';
+let s = Number(a4) + Number(b1);
+console.log(s);
+
+let aaa = 1;
+let bbb = 2;
+let ccc = 3;
+console.log(aaa + bbb + ccc);
